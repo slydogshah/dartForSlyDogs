365b28926d16abcb063c479229862a36c9727f09
babyboy <bugs.bunny.shah@gmail.com>
2020-01-15 21:34:30 -0600

    get_started_ready_to_go

--- aussieOpen.dart
+++ aussieOpen.dart
@@ -1,9 +1,27 @@
-//Import files
+//Import files - Software Terminology
+//Basically means, hook in or integrate the software stored in tennis/tennis.dart file
 import 'tennis/tennis.dart';
 
+//This is the starting point of any Dart Program.
+//All software just like life needs a point where the first line of function is 
+//set into motion
+//Lets just say, "void main" in Dart means, sweet mama giving birth to a beautiful child in life
 void main(List<String> args)
 {
+  //This means, we just created a player named Roger Federer and asked him to hit a forehand
   Tennis roger = new Tennis("Roger Federer");
-
   roger.hitAForeHand();
+
+  //Novak Djokovic - World Number 2. Strong Contender to win the Aussie Open
+  Tennis novak = new Tennis("Novak Djokovic");
+  novak.hitAForeHand();
+
+  //Dominic Thiem - Can hit blinding forehands and backhands. Watch out, he may run away with the title
+  Tennis thiemo = new Tennis("Dominic Thiem");
+  thiemo.hitAForeHand();
+
+  //Andre Agassi - Considered Aussie Open his "unfair" competitive advantage. 
+  //Used to run the hills of Las Vegas till his lungs bled...kinda of course ;)
+  Tennis agassi = new Tennis("Andre Agassi");
+  agassi.hitAForeHand();
 }
--- tennis/tennis.dart
+++ tennis/tennis.dart
@@ -1,17 +1,37 @@
+//Class means indicating the ingredients for creating an actual
+//Object that runs and makes things happen when the Dart Software 
+//actually runs
 class Tennis
 {
   String player;
 
+  //Constructor - also known as the Software that runs when 
+  //an Object is created at runtime
   Tennis(String player)
   {
     this.player = player;
   }
 
+  //This is the Software that runs when "hitAForeHand" is invoked on a Tennis Object
   void hitAForeHand()
   {
+    //All this is self-explanatory
+    //HOMEWORK_ASSIGNMENT: Extend this to your Favorite Players and tell me what you like about them
     if(this.player == "Roger Federer")
     {
       print("$player > Just hit a down the line winner");
+    }
+    else if(this.player == "Novak Djokovic")
+    {
+      print("$player > Just stretched the guy out of the court from either wing");
+    }
+    else if(this.player == "Dominic Thiem")
+    {
+      print("$player > Just...oops what happened, where is the ball. I think Dominic may have the fastest forehand after Juan Martin Del Potro");
+    }
+    else if(this.player == "Andre Agassi")
+    {
+      print("$player > Just...aah..forget it. This guy will run me till my tires bust. Unless of course I am Jimmy Connors, then I am perfectly fine running into the crowd like a Tennis Clown");
     }
   }
 }
