9ffdd644a1e63c07e3e2ad4dafe8e0b1ddb2fbfb
babyboy <bugs.bunny.shah@gmail.com>
2020-01-22 20:49:25 -0600

    quarkus_based_microservice

--- dart-rest-client/src/test/java/io/kittykat/microservice/MicroserviceTest.java
+++ dart-rest-client/src/test/java/io/kittykat/microservice/MicroserviceTest.java
@@ -7,15 +7,14 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
-public class ExampleResourceTest {
+public class MicroserviceTest {
 
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/hello")
+          .when().get("/microservice")
           .then()
-             .statusCode(200)
-             .body(is("hello"));
+             .statusCode(200);
     }
 
 }
